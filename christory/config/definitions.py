bddac011340d910f7f0436ced2b369ee869f6a5d
Gabe <107259694+TrufflePig123@users.noreply.github.com>
2023-04-03 23:39:20 -0400

    Create definitions.py
    Config file for relative path support.

---
+++ christory/config/definitions.py
@@ -0,0 +1,5 @@
+import os
+
+#Thank you https://towardsdatascience.com/simple-trick-to-work-with-relative-paths-in-python-c072cdc9acb9
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__) , '..'))
+
